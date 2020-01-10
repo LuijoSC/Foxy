17f55afe8b878f9d58296a7fd6a5626fe7567d87
Miguel Cabrera <miguel.cabrera.1095@gmail.com>
2020-01-09 18:47:03 -0600

    Tasks can be added using modal. Database working.

--- models/task.js
+++ models/task.js
@@ -1,25 +1,25 @@
-module.exports = function(sequelize, DataTypes) {
-  var Task = sequelize.define ("tasks_fx", {
-    taskId: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    taskInfo: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    taskDescription: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    }
-}, {freezeTableName: true});
-// Generating tables asociation 
-// Task.associate =  function(models) {
-//     Task.belongsTo(Users_fx, {
-//         foreignKey: 'userId'
-//     });
-// };
-return Task;
+module.exports = function (sequelize, DataTypes) {
+    var Task = sequelize.define("Task", {
+        taskId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        taskInfo: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        taskDescription: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    }, { freezeTableName: true });
+    // Generating tables asociation 
+    // Task.associate =  function(models) {
+    //     Task.belongsTo(Users_fx, {
+    //         foreignKey: 'userId'
+    //     });
+    // };
+    return Task;
 };
--- public/js/index.js
+++ public/js/index.js
@@ -22,7 +22,7 @@ let selectedYear = currentYear;
 mth_element.textContent = `${months[currentMonth]} ${currentYear}`;
 
 selected_date_element.textContent = formatDate(currentDate);
-selected_date_element.dataset.value = selectedDate;
+selected_date_element.dataset.value = moment(selectedDate).format('YYYY-MM-DD HH:mm:ss');
 
 // Event listeners
 date_picker_element.addEventListener('click', toggleDatePicker);
@@ -85,10 +85,14 @@ function populateDates(e) {
       selectedDay = parseInt(this.textContent);
       selectedMonth = parseInt(currentMonth);
       selectedYear = parseInt(currentYear);
-      console.log(`${selectedDay}/${selectedMonth}/${selectedYear}`)
       selectedDate = new Date(`${selectedYear}-${selectedMonth + 1}-${parseInt(this.textContent)} `);
       selected_date_element.textContent = formatDate(selectedDate);
-      selected_date_element.dataset.value = selectedDate;
+      selected_date_element.dataset.value = moment(selectedDate).format('YYYY-MM-DD HH:mm:ss');
+      var queryDate = {
+        initialTime: selected_date_element.dataset.value
+      };
+      console.log(queryDate);
+
       populateDates();
     });
 
@@ -156,3 +160,107 @@ function zeller(month, year) {
       return 6
   }
 }
+
+// Get references to page elements
+var $taskText = $("#task-text");
+var $taskDescription = $("#task-description");
+var $submitBtn = $("#submit");
+var $taskList = $("#task-list");
+
+// The API object contains methods for each kind of request we'll make
+var API = {
+  saveTask: function (task) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "api/tasks",
+      data: JSON.stringify(task)
+    });
+  },
+  getTasks: function () {
+    return $.ajax({
+      url: "api/tasks",
+      type: "GET"
+    });
+  },
+  deleteTasks: function (id) {
+    return $.ajax({
+      url: "api/tasks/" + id,
+      type: "DELETE"
+    });
+  }
+};
+
+// refreshTasks gets new tasks from the db and repopulates the list
+var refreshTasks = function () {
+  API.getTasks().then(function (data) {
+    var $tasks = data.map(function (task) {
+      var $a = $("<a>")
+        .text(task.taskInfo)
+        .attr("href", "/task/" + task.taskId);
+
+      var $li = $("<li>")
+        .attr({
+          class: "list-group-item",
+          "data-id": task.taskId
+        })
+        .append($a);
+
+      var $button = $("<button>")
+        .addClass("btn btn-danger float-right delete")
+        .text("ｘ");
+
+      $li.append($button);
+
+      return $li;
+    });
+
+    $taskList.empty();
+    $taskList.append($tasks);
+  });
+};
+
+refreshTasks();
+
+// handleFormSubmit is called whenever we submit a new example
+// Save the new example to the db and refresh the list
+var handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  var task = {
+    taskInfo: $taskText.val().trim(),
+    taskDescription: $taskDescription.val().trim()
+  };
+
+  // if (!(task.text && task.description)) {
+  //   alert("You must enter a task text and description!");
+  //   return;
+  // }
+  // console.log(task);
+
+  API.saveTask(task).then(function () {
+    refreshTasks();
+  });
+
+  $taskText.val("");
+  $taskDescription.val("");
+};
+
+// handleDeleteBtnClick is called when an task's delete button is clicked
+// Remove the task from the db and refresh the list
+var handleDeleteBtnClick = function () {
+  var taskId = $(this)
+    .parent()
+    .attr("data-id");
+
+  API.deleteTasks(taskId).then(function () {
+    refreshTasks();
+  });
+};
+
+
+// Add event listeners to the submit and delete buttons
+$submitBtn.on("click", handleFormSubmit);
+$taskList.on("click", ".delete", handleDeleteBtnClick);
